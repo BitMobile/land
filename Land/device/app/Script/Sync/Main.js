88cdac1644c4598a54f00854a30f9482258f8e94
AVMurach <AVMurach@1cbit.ru>
2014-09-30 19:23:02 +0400

    Дата синхронизации остается после перезапуска приложения

--- Land/device/app/Script/Sync/Main.js
+++ Land/device/app/Script/Sync/Main.js
@@ -1,4 +1,10 @@
-﻿// -------------------- Sync Data ------------
+﻿function OnLoad() {
+	DrawDataReport();
+
+	DrawFtpReport();
+}
+
+// -------------------- Sync Data ------------
 function SyncData() {
 		
 	$.dataSyncReport.Visible = false;
@@ -46,7 +52,26 @@ function SyncDataFinish() {
 	
 	$.Remove("peremOutMovingMain");
 	$.AddGlobal("peremOutMovingMain", null);
+	
+	DrawDataReport()
 }
+
+function DrawDataReport() {
+	 var at = Translate["#at#"];
+	 var date = DB.LastSyncTime.ToString("dd.MM.yy ");
+	 var time = DB.LastSyncTime.ToString(" HH:mm");
+
+	 if (DB.SuccessSync) {
+	  $.dataSyncReport.Text = date + at + time;
+	  $.dataSyncReport.Visible = true;
+	  $.dataSyncError.Visible = false;
+	 } else {
+	  $.dataSyncError.Text = Translate["#error#"] + ": " + date + at + time;
+	  $.dataSyncError.Visible = true;
+	  $.dataSyncReport.Visible = false;
+	 }
+}
+
 
 // -------------------- Sync Ftp ------------
 
@@ -84,6 +109,27 @@ function SyncFtpFinish() {
     Variables.Remove("lastFtpSync");
     Variables.AddGlobal("lastFtpSync", DateTime.Now.ToString("dd MMM yyyy HH:mm"));
     $.ftpSyncReport.Text = $.lastFtpSync;
+    
+    DrawFtpReport();    
+}
+
+function DrawFtpReport() {
+	 var at = Translate["#at#"];
+	 var date = FileSystem.LastSyncTime.ToString("dd.MM.yy ");
+	 var time = FileSystem.LastSyncTime.ToString(" HH:mm");
+
+	 if (FileSystem.SuccessSync) {
+	  $.ftpSyncReport.Text = date + at + time;
+	  $.ftpSyncReport.Visible = true;
+	  $.ftpSyncError.Visible = false;
+	 } else {
+	  if (isDefault(FileSystem.LastSyncTime))
+	   $.ftpSyncError.Text = Translate["#Synchronization_has_not_been_performed#"];
+	  else
+	   $.ftpSyncError.Text = Translate["#error#"] + ": " + date + at + time;
+	  $.ftpSyncError.Visible = true;
+	  $.ftpSyncReport.Visible = false;
+	 }
 }
 
 //-------------------- Change Status Moving ------------
